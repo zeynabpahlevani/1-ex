2416eb7ebd8ef1709ed276e9f07074179e7c7373
zeynab pahlevani <zeynabpahlevani2@gmail.com>
2023-12-18 13:35:38 +0330

    create-react-app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const hello =<h1>welcome to my first code</h1>
+
+root.render(hello);
