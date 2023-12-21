a5d709b9429b23076e9d7a03872d056a86de3fff
zeynab pahlevani <zeynabpahlevani2@gmail.com>
2023-12-21 13:10:04 +0330

    third session 2

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const hello =<h1>welcome to my first code</h1>
-
-root.render(hello);
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/style/s.scss"
+// import myImg1 from "./assets/img/logo192.png";
+// import myImg2 from "./assets/img/logo512.png";
 
-const name = 'zeynab';
-const age = '20';
-const introduce = <h2>Hello, my name is {name} and I am {age} years old </h2>
+// const name = 'zeynab';
+// const age = '20';
+// const introduce = <h2>Hello, my name is {name} and I am {age} years old </h2>
+// const styleSheet = {
+//   color: "green",
+//   fontSize: 80,
+//   backgroundColor: "lightgrey",
+// };
+//  const URL = "https://www.google.com";
+// const friends = ["zynb", "ali", "mahla", "sara"];
+// const sending = true;
+// const friendsJsx = friends.map((friend) => (
+//   <div key={friend} style={styleSheet}>
+//     {friend}
+//     <button style={{display:sending ? 'block' : 'none'}}>falow</button>
+//   </div>
+// ));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(introduce);
+// const sending = true;
+// const myJSX = sending == 'true' && <div>lodinge...</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    {/* <img style={{width: 300, background: 'red'}} src={myImg1} />
+    <img style={{width: 300, background: 'green'}} src={myImg2} /> */}
+  </div>
+);
